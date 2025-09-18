ea262a547dfa9ef19ef34a73a40091b42d053bdc
Hilal Ahmad <hilal@knowpakistan.org>
2025-09-18 13:21:59 +0500

    .gitignore added

--- obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512622b6fa855c718b334fc14111bbacdcd42bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
